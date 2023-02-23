3f0e757ffbb115c1a2762c8c6c922f1fcf8fdd69
Joe W <jweber4213@gmail.com>
2023-02-23 16:06:47 +0900

    fixed button numbers & classes, added vars to script

---
+++ script.js
@@ -0,0 +1,27 @@
+/* SCREEN */
+const displayScreen = document.getElementById('screen-disp');
+
+/* BUTTONS: NUMBERS */
+
+const oneBtn = document.getElementById('one-btn');
+const twoBtn = document.getElementById('two-btn');
+const threeBtn = document.getElementById('three-btn');
+const fourBtn = document.getElementById('four-btn');
+const fiveBtn = document.getElementById('five-btn');
+const sixBtn = document.getElementById('six-btn');
+const sevenBtn = document.getElementById('seven-btn');
+const eightBtn = document.getElementById('eight-btn');
+const nineBtn = document.getElementById('nine-btn');
+const zeroBtn = document.getElementById('zero-btn');
+
+/* BUTTONS: OPERATORS */
+
+const clearBtn = document.getElementById('clear-btn');
+const delBtn = document.getElementById('del-btn');
+const expBtn = document.getElementById('exp-btn');
+const rootBtn = document.getElementById('root-btn');
+const modBtn = document.getElementById('mod-btn');
+const divideBtn = document.getElementById('divide-btn');
+const multiBtn = document.getElementById('multi-btn');
+const minusBtn = document.getElementById('minus-btn');
+const plusBtn = document.getElementById('plus-btn');
